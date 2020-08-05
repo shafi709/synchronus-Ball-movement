68e2622f88af79396481d87dac867a634723b2c9
shafi709 <67061112+shafi709@users.noreply.github.com>
2020-08-05 17:49:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var hypnoticball,database,position;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    hypnoticball = createSprite(250,250,10,10);
+    hypnoticball.shapeColor = "blue";
+
+    var hypnoticballposition = database.ref('ball/position')
+    hypnoticballposition.on("value",readposition,showerror)
+}
+
+
+
+function draw(){
+    background("white");
+    if(position !== undefined)
+
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+   database.ref('ball/position').set({
+       'x':position.x+x,
+       'y':position.y+y
+   })
+}
+
+function readposition(data){
+    position = data.val();
+    hypnoticball.x = position.x;
+    hypnoticball.y = position.y;
+}
+function showerror(){
+    console.log("database is not connected")
+}
